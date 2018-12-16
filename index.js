9366889e92809a432e50e1b39def34288616a6ca
Melissa Epperson <Melissa.epperson67@gmail.com>
2018-12-16 12:26:48 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  
+  //Skills section
+    $('#ps').hover(function() {
+      $(this).html('Intermediate');
+    }, function() {
+      $(this).html('Adobe Photoshop');
+    });
+  $('#ai').hover(function() {
+      $(this).html('Intermediate');
+    }, function() {
+      $(this).html('Adobe Illustrator');
+    });
+  $('#ht').hover(function() {
+      $(this).html('Intermediate');
+    }, function() {
+      $(this).html('HTML5');
+    });
+  $('#cs').hover(function() {
+      $(this).html('Intermediate');
+    }, function() {
+      $(this).html('CSS');
+    });
+  $('#js').hover(function() {
+      $(this).html('Beginner');
+    }, function() {
+      $(this).html('JavaScript');
+    });
+  $('#jq').hover(function() {
+      $(this).html('Beginner');
+    }, function() {
+      $(this).html('jQuery');
+    });
+  $('#bs').hover(function() {
+      $(this).html('Beginner');
+    }, function() {
+      $(this).html('Bootstrap');
+    });
+  $('#sql').hover(function() {
+      $(this).html('Beginner');
+    }, function() {
+      $(this).html('SQL');
+    });
+  });
+
